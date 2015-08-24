d2d38cdc0893027f602d2a75af94df16edaac938
Vlad Spreys <vlad@spreys.com>
2015-08-24 20:56:46 +1200

    Added code to retrieve all subcategories from the API.

---
+++ app/src/androidTest/java/com/spreys/trademeviewer/TestNetworkingCalls.java
@@ -0,0 +1,32 @@
+package com.spreys.trademeviewer;
+
+import android.test.AndroidTestCase;
+
+import com.spreys.trademeviewer.Model.Category;
+import com.spreys.trademeviewer.NetworkCommunication.TradeMeApiWrapper;
+
+import java.util.List;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class TestNetworkingCalls extends AndroidTestCase {
+
+    public void testGetAllCategories() {
+        TradeMeApiWrapper apiWrapper = new TradeMeApiWrapper();
+
+        List<Category> categories = apiWrapper.getCategories();
+
+        assertEquals(true, categories.size() > 5);
+        assertEquals(true, categories.get(0).getSubcategories().size() > 3);
+    }
+
+    public void testGetTradeMeMotors() {
+        TradeMeApiWrapper apiWrapper = new TradeMeApiWrapper();
+
+        List<Category> categories = apiWrapper.getCategories("0001-");
+
+        assertEquals(13, categories.size());
+        assertEquals("Aircraft", categories.get(0).getName());
+    }
+}
--- app/src/main/java/com/spreys/trademeviewer/NetworkCommunication/TradeMeApiWrapper.java
+++ app/src/main/java/com/spreys/trademeviewer/NetworkCommunication/TradeMeApiWrapper.java
@@ -2,6 +2,15 @@ package com.spreys.trademeviewer.NetworkCommunication;
 
 import com.spreys.trademeviewer.Model.Category;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 /**
@@ -13,17 +22,105 @@ import java.util.List;
  *         Contact by: vlad@spreys.com
  */
 public class TradeMeApiWrapper {
+    private final static String BASE_API_URL = "https://api.trademe.co.nz/v1/";
+    private final static String API_URL_CATEGORIES = "categories";
+    private final static String API_FORMAT_JSON = ".json";
+    private final static String API_FORMAT_XML = ".xml";
+
+    private boolean useJson = true;
 
     /**
-     * Return categories in the from the root level.
+     * Returns the currently selected API format. Can be modified by setting setApiFormat() method.
+     * Defaults to JSON.
+     * @return Extension to the API format as string. Either ".json" or ".xml"
+     */
+    private String apiFormat() {
+        if(useJson) {
+            return API_FORMAT_JSON;
+        } else {
+            return API_FORMAT_XML;
+        }
+    }
+
+    /**
+     * Method allows to set the messages format returned from the API to JSON
+     */
+    public void useJson(){
+        this.useJson = true;
+    }
+
+    /**
+     * Method allows to set the messages formt returned from the API to XML
+     */
+    public void useXml() {
+        this.useJson = false;
+    }
+
+    /**
+     * Returns all of the categories from the TradeMe API.
+     * @return the list of categories.
      */
     public List<Category> getCategories() {
+        String url = BASE_API_URL + API_URL_CATEGORIES + apiFormat();
+        return new Category(getStringFromUrl(url)).getSubcategories();
+    }
+
+    /**
+     * Returns the list of categories for the supplied category id
+     * @param categoryId Has to be a valid TradeMe category id
+     * @return list of subcategories for the supplied category id
+     */
+    public List<Category> getCategories(String categoryId){
+        String url = BASE_API_URL + API_URL_CATEGORIES + "/" + categoryId + apiFormat();
+        return new Category(getStringFromUrl(url)).getSubcategories();
+    }
+
+    private JSONObject getJsonObjectFromUrl(String url) {
+        try {
+            return new JSONObject(getStringFromUrl(url));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    public List<Category> getCategories(int categoryId){
+    private String getStringFromUrl(String url) {
+        URL myURL;
+        try {
+            myURL = new URL(url);
+            URLConnection conn = myURL.openConnection();
+            conn.setDoOutput(false);
+            conn.setDoInput(true);
+
+            InputStream is = conn.getInputStream();
+            return convertStreamToString(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
 
 }
